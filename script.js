9efcfefe3334313255b046d6496fcae19baf549b
Maite Cardoso <maite_cardoso26@hotmail.com>
2022-07-27 18:17:03 -0300

    entregable crear un algoritmo utilizando un ciclo

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
-function calculadora(primerNumero, segundoNumero, operacion) {
-    switch (operacion) {
-        case "+":
-            return primerNumero + segundoNumero;
-            break;
-        case "-":
-            return primerNumero - segundoNumero;
-            break;
-        case "*":
-            return primerNumero * segundoNumero;
-            break;
-        case "/":
-            return primerNumero / segundoNumero;
-            break;
-        default:
-            return 0;
-            break;
-    }
-}
-console.log(calculadora(10, 5, "*"));
+alert("Buenos días, mi nombre es Maite y te ayudaré a elegir el mejor producto. ¿Cómo es tu nombre? ");
 
-class Persona {
-    constructor(usuario, correo, password){
-        this.usuario = usuario;
-        this.correo = correo;
-        this.password = password;
-    }
-     
+let nombre = prompt("Por favor, ingresá tu nombre: ");
+
+function saludar(){
+    alert("Bienvenido/a " + nombre + " a EstePan!");
 }
 
-// const persona1 = new Persona("Juan", "juan@gmail.com", 1234);
+saludar();
 
-// console.log(persona1)
-// alert(persona1.usuario + " " + persona1.password  + " " + persona1.correo)
+alert("Ahora, necesito que me digas un número del 1 al 50, y te ganarás un beneficio: ");
 
-const persona2 = new Persona(prompt("ingrese su nombre de ususario"), prompt("ingrese su email"), prompt("ingrese su password"));
-console.log(persona2)
-alert(persona2.usuario + " " + persona2.password  + " " + persona2.correo)
+let numero = parseInt(prompt("Ingresa un número del 1 al 50"));
 
+alert("Excelente, recibiste un descuento de:  " + numero + "  %.");
